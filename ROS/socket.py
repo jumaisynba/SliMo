5ec3b48042ed4ba5fa8ceee39527025a673d5a8e
jumaisynba <46604270+jumaisynba@users.noreply.github.com>
2021-02-05 17:59:08 +0600

    minor socket updates

--- ROS/socket.py
+++ ROS/socket.py
@@ -2,7 +2,7 @@
 import socket
 import sys
 import rospy
-import json
+#import json
 from geometry_msgs.msg import Wrench, WrenchStamped
 from std_msgs.msg import Float64
 
@@ -10,18 +10,18 @@ from std_msgs.msg import Float64
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ("192.168.2.114", 8052)
-print (sys.stderr, "starting up on %s port %s" % server_address)
+server_address = ("10.1.70.221", 8052)
+print(sys.stderr, "starting up on %s port %s" % server_address)
 
 
-sock.bind(("192.168.2.150", 8052))
+sock.bind(("10.1.70.221", 8052))
 
 # Listen for incoming connections
 sock.listen(1)
 
-print (sys.stderr, "waiting for a connection")
+print(sys.stderr, "waiting for a connection")
 connection, client_address = sock.accept()
-print (sys.stderr, "connection from", client_address)
+print(sys.stderr, "connection from", client_address)
 
 
 class Nodo():
@@ -30,27 +30,29 @@ class Nodo():
         sub = rospy.Subscriber('/wrench',
                                WrenchStamped, self.callback)
 
-        print ("I was here")
-        print (sub)
+        print("I was here")
+        print(sub)
         rospy.spin()
 
     def callback(self, data):
         self.position = data.wrench.force.z
-        print (self.position)
+        print(self.position)
         try:
-            connection.send('|'+str(self.position))
+            connection.send('|'.encode()+str(self.position).encode())
         except:
             connection.close()
             print("closing connection")
+             
         # ------------try sending data
-        #connection.send("I have position")
+        # connection.send("I have position")
         # finally:
         # Clean up the connection
         #
 
         # rospy.spin()
 
+
 if __name__ == '__main__':
     rospy.init_node('wearami_socket', anonymous=True)
-    print ("also here")
+    print("also here")
     Nodo()
